733d89d2fd16130e2cdadf1b25d7145b3479bda9
mfigand <m.figand@gmail.com>
2020-04-11 15:29:11 +0200

    Add createLineForm to profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../shared/auth.service';
 import { User } from '../shared/user';
@@ -9,6 +11,10 @@ import { User } from '../shared/user';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  createLineForm: FormGroup;
+  loading = false;
+  showCreateLine = false;
+  submitted = false;
   currentUser: User = { id: '',
                         name: '',
                         lastname: '',
@@ -16,7 +22,9 @@ export class ProfileComponent implements OnInit {
                         password: '' };
 
   constructor(
-    private authService: AuthService
+    private formBuilder: FormBuilder,
+    public authService: AuthService,
+    public router: Router
   ) { 
     this.authService.currentUser.subscribe(res => {
         if (res) { this.currentUser = res }
@@ -24,6 +32,37 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.createLineForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      protagonist: ['']
+    });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.createLineForm.controls; }
+
+  toggleCreateLineForm(){
+    this.showCreateLine = this.showCreateLine ? false : true;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.createLineForm.invalid) {
+        return;
+    }
+
+    this.loading = true;
+
+    // this.authService.signIn(this.createLineForm.value).subscribe((res) => {
+    //   localStorage.setItem('access_token', res.data)
+    //   let tokenInfo = this.authService.getDecodedAccessToken(res.data)
+    //   this.authService.getUserProfile(tokenInfo.user_id).subscribe((res) => {
+    //     this.router.navigate(['profile']);
+    //   })
+    // })
+
+    this.loading = false;
+  }
 }
